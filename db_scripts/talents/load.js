f96bedb6b6270baa5e7b97484e5ce96f4b844279
samk3n <neubeck.theo@gmail.com>
2024-03-21 18:27:39 +0100

    Completed filling talents data.txt + added db record creation in load.js for talents bulk creation

--- db_scripts/talents/load.js
+++ db_scripts/talents/load.js
@@ -1,6 +1,16 @@
 import { open } from 'node:fs/promises';
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('https://warhammer.line.pm:3001');
+await pb.admins.authWithPassword('neubeck.theo@gmail.com', 'A10t30&3119');
 
 const file = await open("data.txt");
 for await (const line of file.readLines()) {
-    console.log(line.split(';'))
+    const split = line.split(';')
+
+    const record = await pb.collection('talents').create({
+        id: split[0],
+        name: split[1],
+        description: split[2],
+    });   
 }
